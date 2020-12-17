892e43c85e5fe43a511f8270ad860045540ca558
FaranushKarimov <faranush.karimov@gmail.com>
2020-12-17 23:42:46 +0300

    hw 31

--- cmd/main.go
+++ cmd/main.go
@@ -2,71 +2,55 @@ package main
 
 import (
 	"context"
+	"log"
+	"net"
+	"net/http"
+	"os"
 	"time"
 
 	"github.com/FaranushKarimov/crud/cmd/app"
 	"github.com/FaranushKarimov/crud/pkg/customers"
+	"github.com/FaranushKarimov/crud/pkg/customers/security"
 	"github.com/gorilla/mux"
-	"go.uber.org/dig"
-
-	"log"
-	"net/http"
-	"os"
-
 	"github.com/jackc/pgx/v4/pgxpool"
+	"go.uber.org/dig"
 )
 
+func init() {
+	log.SetFlags(log.Llongfile)
+}
+
 func main() {
-	//это хост
 	host := "0.0.0.0"
-	//это порт
 	port := "9999"
-	//это строка подключения к бд
-	dbConnectionString := "postgres://app:pass@localhost:5432/db"
-	//запускаем функцию execute c проверкой на err
-	if err := execute(host, port, dbConnectionString); err != nil {
-		//если получили ошибку то закрываем приложения
+	dsn := "postgres://app:pass@192.168.1.186:5433/db"
+
+	if err := execute(host, port, dsn); err != nil {
 		log.Print(err)
 		os.Exit(1)
 	}
 }
 
-//функция запуска сервера
-func execute(host, port, dbConnectionString string) (err error) {
-
-	//здес обявляем слайс с зависимостями то есть добавляем все сервисы и конструкторы
-	dependencies := []interface{}{
-		app.NewServer,
-		mux.NewRouter,
-		func() (*pgxpool.Pool, error) {
-			connCtx, _ := context.WithTimeout(context.Background(), time.Second*5)
-			return pgxpool.Connect(connCtx, dbConnectionString)
-		},
-		customers.NewService,
-		func(server *app.Server) *http.Server {
-			return &http.Server{
-				Addr:    host + ":" + port,
-				Handler: server,
-			}
-		},
-	}
-
-	//обявляем новый контейнер
+func execute(host, port, dsn string) (err error) {
 	container := dig.New()
-	//в цикле регистрируем все зависимостив контейнер
-	for _, v := range dependencies {
-		err = container.Provide(v)
-		if err != nil {
-			return err
-		}
-	}
-
-	/*вызываем метод Invoke позволяет вызвать в контейнере функцию*/
-	err = container.Invoke(func(server *app.Server) {
-		server.Init()
+	container.Provide(app.NewServer)
+	container.Provide(mux.NewRouter)
+	container.Provide(func() (*pgxpool.Pool, error) {
+		connCtx, _ := context.WithTimeout(context.Background(), time.Second*5)
+		return pgxpool.Connect(connCtx, dsn)
 	})
-	//если получили ошибку то вернем его
-	if err != nil {
+	container.Provide(customers.NewService)
+	container.Provide(func(server *app.Server) *http.Server {
+		return &http.Server{
+			Addr:    net.JoinHostPort(host, port),
+			Handler: server,
+		}
+	})
+	container.Provide(security.NewService)
+
+	if err := container.Invoke(func(server *app.Server) {
+		server.Init()
+	}); err != nil {
 		return err
 	}
 
