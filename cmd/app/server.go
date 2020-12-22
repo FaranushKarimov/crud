4f805f4004d48c63699276a0ce0d050c10c4401c
faranush <faranush.karimov@gmail.com>
2020-12-22 10:40:51 +0500

    hw 32

--- cmd/app/server.go
+++ cmd/app/server.go
@@ -7,283 +7,409 @@ import (
 	"net/http"
 	"strconv"
 
-	"github.com/FaranushKarimov/crud/cmd/app/middleware"
 	"github.com/FaranushKarimov/crud/pkg/customers"
-	"github.com/FaranushKarimov/crud/pkg/customers/security"
+	"github.com/FaranushKarimov/crud/pkg/security"
 	"github.com/gorilla/mux"
+	"golang.org/x/crypto/bcrypt"
 )
 
-// Server ... sfisndfgi
+// Server ...
 type Server struct {
 	mux          *mux.Router
 	customersSvc *customers.Service
-	security     *security.Service
+	securitySvc  *security.Service
 }
 
 // NewServer ...
-func NewServer(mux *mux.Router, customersSvc *customers.Service, security *security.Service) *Server {
-	return &Server{
-		mux:          mux,
-		customersSvc: customersSvc,
-		security:     security,
-	}
+func NewServer(mux *mux.Router, customersSvc *customers.Service, securitySvc *security.Service) *Server {
+	return &Server{mux: mux, customersSvc: customersSvc, securitySvc: securitySvc}
 }
 
-// ServerHTTP ...
-func (s *Server) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
-	s.mux.ServeHTTP(rw, r)
+// ServeHTTP ...
+func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	s.mux.ServeHTTP(w, r)
 }
+
+const (
+	// GET ...
+	GET = "GET"
+	// POST ...
+	POST = "POST"
+	// DELETE ...
+	DELETE = "DELETE"
+)
 
 // Init ...
 func (s *Server) Init() {
-	s.mux.Use(middleware.Logger)
-	auth := middleware.Basic(s.security.Auth)
-	s.mux.Use(auth)
-	chMd := middleware.CheckHeader("Content-Type", "application/json")
+	// s.mux.HandleFunc("/customers.getAll", s.handleGetAllCustomers)
+	s.mux.HandleFunc("/customers", s.handleGetAllCustomers).Methods(GET)
 
-	s.mux.HandleFunc("/customers", s.handleGetAllCustomers).Methods("GET")
-	s.mux.Handle("/customers", chMd(http.HandlerFunc(s.handleSaveCustomer))).Methods("POST")
-	s.mux.HandleFunc("/customers/active", s.handleGetAllActiveCustomers).Methods("GET")
-	s.mux.HandleFunc("/customers/{id}", s.handleGetCustomerByID).Methods("GET")
-	s.mux.HandleFunc("/customers/{id}", s.handleRemoveCustomerByID).Methods("DELETE")
-	s.mux.HandleFunc("/customers/{id}/block", s.handleBlockCustomerByID).Methods("POST")
-	s.mux.HandleFunc("/customers/{id}/block", s.handleUnblockCustomerByID).Methods("DELETE")
+	// s.mux.HandleFunc("/customers.getAllActive", s.handleGetAllActiveCustomers)
+	s.mux.HandleFunc("/customers/active", s.handleGetAllActiveCustomers).Methods(GET)
+
+	// s.mux.HandleFunc("/customers.getById", s.handleGetCustomerByID)
+	s.mux.HandleFunc("/customers/{id}", s.handleGetCustomerByID).Methods(GET)
+
+	// s.mux.HandleFunc("/customers.save", s.handleSaveCustomer)
+	// s.mux.HandleFunc("/customers", s.handleSaveCustomer).Methods(POST)
+
+	// s.mux.HandleFunc("/customers.removeById", s.handleRemoveByID)
+	s.mux.HandleFunc("/customers/{id}", s.handleRemoveByID).Methods(DELETE)
+
+	// s.mux.HandleFunc("/customers.blockById", s.handleBlockByID)
+	s.mux.HandleFunc("/customers/{id}/block", s.handleBlockByID).Methods(POST)
+
+	// s.mux.HandleFunc("/customers.unblockById", s.handleUnblockByID)
+	s.mux.HandleFunc("/customers/{id}/block", s.handleUnblockByID).Methods(DELETE)
+
+	// s.mux.Use(middleware.Basic(s.securitySvc.Auth))
+
+	s.mux.HandleFunc("/api/customers", s.handleSaveCustomer).Methods(POST)
+	s.mux.HandleFunc("/api/customers/token", s.handleCreateToken).Methods(POST)
+	s.mux.HandleFunc("/api/customers/token/validate", s.handleValidateToken).Methods(POST)
 }
 
-func (s *Server) handleGetCustomerByID(rw http.ResponseWriter, r *http.Request) {
+func (s *Server) handleGetCustomerByID(w http.ResponseWriter, r *http.Request) {
 	idParam, ok := mux.Vars(r)["id"]
+
 	if !ok {
-		http.Error(rw, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
 		return
 	}
 
 	id, err := strconv.ParseInt(idParam, 10, 64)
 	if err != nil {
 		log.Print(err)
-		http.Error(rw, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
 		return
 	}
 
 	item, err := s.customersSvc.ByID(r.Context(), id)
+	if errors.Is(err, customers.ErrNotFound) {
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+		return
+	}
+
 	if err != nil {
-		log.Print(err)
-		if errors.Is(err, customers.ErrNotFound) {
-			http.Error(rw, http.StatusText(http.StatusNotFound), http.StatusNotFound)
-			return
-		}
-		http.Error(rw, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 		return
 	}
 
 	data, err := json.Marshal(item)
 	if err != nil {
 		log.Print(err)
-		http.Error(rw, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 		return
 	}
 
-	rw.Header().Set("Content-Type", "application/json")
-	_, err = rw.Write(data)
+	w.Header().Set("Content-Type", "application/json")
+	_, err = w.Write(data)
 	if err != nil {
 		log.Print(err)
 	}
 }
 
-func (s *Server) handleGetAllCustomers(rw http.ResponseWriter, r *http.Request) {
-	items, err := s.customersSvc.All(r.Context())
-	if err != nil {
-		log.Print(err)
-		if errors.Is(err, customers.ErrNotFound) {
-			http.Error(rw, http.StatusText(http.StatusNotFound), http.StatusNotFound)
-			return
-		}
-		http.Error(rw, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+func (s *Server) handleGetAllCustomers(w http.ResponseWriter, r *http.Request) {
+	item, err := s.customersSvc.All(r.Context())
+	if errors.Is(err, customers.ErrNotFound) {
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
 		return
 	}
 
-	data, err := json.Marshal(items)
 	if err != nil {
-		log.Print(err)
-		http.Error(rw, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-		return
-	}
-
-	rw.Header().Set("Content-Type", "application/json")
-	_, err = rw.Write(data)
-	if err != nil {
-		log.Print(err)
-	}
-}
-
-func (s *Server) handleGetAllActiveCustomers(rw http.ResponseWriter, r *http.Request) {
-	items, err := s.customersSvc.AllActive(r.Context())
-	if err != nil {
-		log.Print(err)
-		if errors.Is(err, customers.ErrNotFound) {
-			http.Error(rw, http.StatusText(http.StatusNotFound), http.StatusNotFound)
-			return
-		}
-		http.Error(rw, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-		return
-	}
-
-	data, err := json.Marshal(items)
-	if err != nil {
-		log.Print(err)
-		http.Error(rw, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-		return
-	}
-
-	rw.Header().Set("Content-Type", "application/json")
-	_, err = rw.Write(data)
-	if err != nil {
-		log.Print(err)
-	}
-}
-
-func (s *Server) handleSaveCustomer(rw http.ResponseWriter, r *http.Request) {
-	var itemToSave *customers.Customer
-
-	if err := json.NewDecoder(r.Body).Decode(&itemToSave); err != nil {
-		log.Print(err)
-		http.Error(rw, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
-		return
-	}
-
-	item, err := s.customersSvc.Save(r.Context(), itemToSave)
-	if err != nil {
-		log.Print(err)
-		http.Error(rw, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 		return
 	}
 
 	data, err := json.Marshal(item)
 	if err != nil {
 		log.Print(err)
-		http.Error(rw, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 		return
 	}
 
-	rw.Header().Set("Content-Type", "application/json")
-	_, err = rw.Write(data)
+	w.Header().Set("Content-Type", "application/json")
+	_, err = w.Write(data)
+	if err != nil {
+		log.Print(err)
+		return
+	}
+}
+
+func (s *Server) handleGetAllActiveCustomers(w http.ResponseWriter, r *http.Request) {
+
+	item, err := s.customersSvc.AllActive(r.Context())
+	if errors.Is(err, customers.ErrNotFound) {
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+		return
+	}
+
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	data, err := json.Marshal(item)
+	if err != nil {
+		log.Print(err)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	_, err = w.Write(data)
+	if err != nil {
+		log.Print(err)
+		return
+	}
+}
+
+func (s *Server) handleSaveCustomer(w http.ResponseWriter, r *http.Request) {
+	var customer *customers.Customer
+
+	err := json.NewDecoder(r.Body).Decode(&customer)
+	if err != nil {
+		log.Print(err)
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+		return
+	}
+
+	hash, err := bcrypt.GenerateFromPassword([]byte(customer.Password), bcrypt.DefaultCost)
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+	customer.Password = string(hash)
+
+	item, err := s.customersSvc.Save(r.Context(), customer)
+	if err != nil {
+		log.Print(err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	data, err := json.Marshal(item)
+	if err != nil {
+		log.Print(err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	_, err = w.Write(data)
 	if err != nil {
 		log.Print(err)
 	}
 }
 
-func (s *Server) handleRemoveCustomerByID(rw http.ResponseWriter, r *http.Request) {
+func (s *Server) handleRemoveByID(w http.ResponseWriter, r *http.Request) {
 	idParam, ok := mux.Vars(r)["id"]
+
 	if !ok {
-		http.Error(rw, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
 		return
 	}
 
 	id, err := strconv.ParseInt(idParam, 10, 64)
 	if err != nil {
 		log.Print(err)
-		http.Error(rw, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
 		return
 	}
 
 	item, err := s.customersSvc.RemoveByID(r.Context(), id)
+	if errors.Is(err, customers.ErrNotFound) {
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+		return
+	}
+
 	if err != nil {
-		log.Print(err)
-		if errors.Is(err, customers.ErrNotFound) {
-			http.Error(rw, http.StatusText(http.StatusNotFound), http.StatusNotFound)
-			return
-		}
-		http.Error(rw, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 		return
 	}
 
 	data, err := json.Marshal(item)
 	if err != nil {
 		log.Print(err)
-		http.Error(rw, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 		return
 	}
 
-	rw.Header().Set("Content-Type", "application/json")
-	_, err = rw.Write(data)
+	w.Header().Set("Content-Type", "application/json")
+	_, err = w.Write(data)
 	if err != nil {
 		log.Print(err)
+		return
 	}
 }
 
-func (s *Server) handleBlockCustomerByID(rw http.ResponseWriter, r *http.Request) {
+func (s *Server) handleBlockByID(w http.ResponseWriter, r *http.Request) {
 	idParam, ok := mux.Vars(r)["id"]
+
 	if !ok {
-		http.Error(rw, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
 		return
 	}
 
 	id, err := strconv.ParseInt(idParam, 10, 64)
 	if err != nil {
 		log.Print(err)
-		http.Error(rw, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
 		return
 	}
 
 	item, err := s.customersSvc.BlockByID(r.Context(), id)
+	if errors.Is(err, customers.ErrNotFound) {
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+		return
+	}
+
 	if err != nil {
-		log.Print(err)
-		if errors.Is(err, customers.ErrNotFound) {
-			http.Error(rw, http.StatusText(http.StatusNotFound), http.StatusNotFound)
-			return
-		}
-		http.Error(rw, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 		return
 	}
 
 	data, err := json.Marshal(item)
 	if err != nil {
 		log.Print(err)
-		http.Error(rw, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 		return
 	}
 
-	rw.Header().Set("Content-Type", "application/json")
-	_, err = rw.Write(data)
+	w.Header().Set("Content-Type", "application/json")
+	_, err = w.Write(data)
 	if err != nil {
 		log.Print(err)
+		return
 	}
 }
 
-func (s *Server) handleUnblockCustomerByID(rw http.ResponseWriter, r *http.Request) {
+func (s *Server) handleUnblockByID(w http.ResponseWriter, r *http.Request) {
 	idParam, ok := mux.Vars(r)["id"]
+
 	if !ok {
-		http.Error(rw, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
 		return
 	}
 
 	id, err := strconv.ParseInt(idParam, 10, 64)
 	if err != nil {
 		log.Print(err)
-		http.Error(rw, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
 		return
 	}
 
 	item, err := s.customersSvc.UnblockByID(r.Context(), id)
+	if errors.Is(err, customers.ErrNotFound) {
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+		return
+	}
+
 	if err != nil {
-		log.Print(err)
-		if errors.Is(err, customers.ErrNotFound) {
-			http.Error(rw, http.StatusText(http.StatusNotFound), http.StatusNotFound)
-			return
-		}
-		http.Error(rw, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 		return
 	}
 
 	data, err := json.Marshal(item)
 	if err != nil {
 		log.Print(err)
-		http.Error(rw, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 		return
 	}
 
-	rw.Header().Set("Content-Type", "application/json")
-	_, err = rw.Write(data)
+	w.Header().Set("Content-Type", "application/json")
+	_, err = w.Write(data)
+	if err != nil {
+		log.Print(err)
+		return
+	}
+}
+
+func (s *Server) handleCreateToken(w http.ResponseWriter, r *http.Request) {
+	var customer *struct {
+		Login    string `json:"login"`
+		Password string `json:"password"`
+	}
+
+	err := json.NewDecoder(r.Body).Decode(&customer)
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+		return
+	}
+
+	token, err := s.securitySvc.TokenForCustomer(r.Context(), customer.Login, customer.Password)
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+		return
+
+	}
+
+	response := map[string]interface{}{"status": "ok", "token": token}
+
+	data, err := json.Marshal(response)
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	_, err = w.Write(data)
 	if err != nil {
 		log.Print(err)
 	}
+}
+
+func (s *Server) handleValidateToken(w http.ResponseWriter, r *http.Request) {
+	var customer *struct {
+		Token string `json:"token"`
+	}
+
+	err := json.NewDecoder(r.Body).Decode(&customer)
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+		return
+	}
+
+	id, err := s.securitySvc.AuthenticateCustomer(r.Context(), customer.Token)
+	if err != nil {
+		// status := http.StatusInternalServerError
+		status := http.StatusNotFound
+		errText := "not found"
+
+		if err == security.ErrNoSuchUser {
+			status = http.StatusNotFound
+			errText = "not found"
+		}
+		if err == security.ErrExpireToken {
+			status = http.StatusBadRequest
+			errText = "expired"
+		}
+
+		response := map[string]interface{}{"status": "fail", "reason": errText}
+		data, err := json.Marshal(response)
+		if err != nil {
+			http.Error(w, http.StatusText(status), status)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(status)
+		// w.WriteHeader(http.StatusOK)
+		_, err = w.Write(data)
+		if err != nil {
+			log.Print(err)
+			return
+		}
+		return
+	}
+
+	response := make(map[string]interface{})
+	response["status"] = "ok"
+	response["customerId"] = id
+
+	data, err := json.Marshal(response)
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	_, err = w.Write(data)
+	if err != nil {
+		log.Print(err)
+	}
+
 }
--- cmd/main.go
+++ cmd/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"log"
 	"net"
 	"net/http"
 	"os"
@@ -10,51 +9,61 @@ import (
 
 	"github.com/FaranushKarimov/crud/cmd/app"
 	"github.com/FaranushKarimov/crud/pkg/customers"
-	"github.com/FaranushKarimov/crud/pkg/customers/security"
+	"github.com/FaranushKarimov/crud/pkg/security"
 	"github.com/gorilla/mux"
 	"github.com/jackc/pgx/v4/pgxpool"
 	"go.uber.org/dig"
 )
 
-func init() {
-	log.SetFlags(log.Llongfile)
-}
-
+// main function
 func main() {
 	host := "0.0.0.0"
 	port := "9999"
-	dsn := "postgres://app:pass@192.168.1.186:5433/db"
+	dsn := "postgres://app:pass@192.168.99.100:5432/db"
 
 	if err := execute(host, port, dsn); err != nil {
-		log.Print(err)
 		os.Exit(1)
 	}
+
 }
 
-func execute(host, port, dsn string) (err error) {
-	container := dig.New()
-	container.Provide(app.NewServer)
-	container.Provide(mux.NewRouter)
-	container.Provide(func() (*pgxpool.Pool, error) {
-		connCtx, _ := context.WithTimeout(context.Background(), time.Second*5)
-		return pgxpool.Connect(connCtx, dsn)
-	})
-	container.Provide(customers.NewService)
-	container.Provide(func(server *app.Server) *http.Server {
-		return &http.Server{
-			Addr:    net.JoinHostPort(host, port),
-			Handler: server,
-		}
-	})
-	container.Provide(security.NewService)
+//execute function
+func execute(host string, port string, dsn string) (err error) {
 
-	if err := container.Invoke(func(server *app.Server) {
+	deps := []interface{}{
+		app.NewServer,
+		mux.NewRouter,
+		func() (*pgxpool.Pool, error) {
+			ctx, _ := context.WithTimeout(context.Background(), time.Second*5)
+			return pgxpool.Connect(ctx, dsn)
+		},
+		customers.NewService,
+		security.NewService,
+		func(server *app.Server) *http.Server {
+			return &http.Server{
+				Addr:    net.JoinHostPort(host, port),
+				Handler: server,
+			}
+		},
+	}
+
+	container := dig.New()
+	for _, dep := range deps {
+		err = container.Provide(dep)
+		if err != nil {
+			return err
+		}
+	}
+
+	err = container.Invoke(func(server *app.Server) {
 		server.Init()
-	}); err != nil {
+	})
+	if err != nil {
 		return err
 	}
 
 	return container.Invoke(func(server *http.Server) error {
 		return server.ListenAndServe()
 	})
+
 }
